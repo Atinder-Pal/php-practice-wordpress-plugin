ffadf4fabdcf6a81dd42a713b918edff2350fb8e
Atinder-Pal <atty.pal46@gmail.com>
2020-10-07 14:36:56 -0600

    Initial commit

---
+++ techcareers-calc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       TECHCareers Calculator
+ * Plugin URI:        https://TECHCareers.ca
+ * Description:       A simple PHP calculator
+ * Version:           1.0
+ * Requires at least: 5.
+ * Requires PHP:      7.4
+ * Author:            Atinder Pal 
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html 
+ */
